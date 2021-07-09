ccb14119440732c7b05c0f4de65f67c8b083ce30
acorazza <alessandro@corazzamarketing.com>
2021-07-09 14:58:49 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,24 +1,28 @@
 <?php
 
 /**
-* Plugin Name: Corazza Custom Functions
-* Description: This plugin contains custom functions for the website theme.
-* Author: Alessandro Corazza
-* Version: 1.0
-*/
+ * Plugin Name: Corazza Custom Functions
+ * Description: This plugin contains custom functions for the website theme.
+ * Author: Alessandro Corazza
+ * Version: 1.0
+ */
 
 // 🔍🔍🔍 Google Analytics Tracking Code
 
-add_action( 'wp_head', 'ps_google_analytics' );
+add_action('wp_head', 'ps_google_analytics');
 
-function ps_google_analytics() { ?>
+function ps_google_analytics()
+{ ?>
       <!-- Global site tag (gtag.js) - Google Analytics -->
-            <script async src=""></script>
-            <script>
+      <script async src=""></script>
+      <script>
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
+
+            function gtag() {
+                  dataLayer.push(arguments);
+            }
             gtag('js', new Date());
 
             gtag('config', 'UA-26515983-1');
-            </script>
+      </script>
 <?php }
