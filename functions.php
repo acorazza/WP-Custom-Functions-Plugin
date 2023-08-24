49067efcb41065ff63e99d967afe7637dd03129a
Alessandro Corazza <alessandro@corazzamarketing.com>
2023-08-24 20:19:29 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,22 +7,4 @@
  * Version: 1.0
  */
 
-// 🔍🔍🔍 Google Analytics Tracking Code
-
-add_action('wp_head', 'ps_google_analytics');
-
-function ps_google_analytics()
-{ ?>
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-      <script async src=""></script>
-      <script>
-            window.dataLayer = window.dataLayer || [];
-
-            function gtag() {
-                  dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-
-            gtag('config', 'UA-26515983-1');
-      </script>
-<?php }
+// ADD YOUR PHP HERE
